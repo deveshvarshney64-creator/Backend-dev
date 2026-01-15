86245d65b517392defad3ca2e58d8cee4bbc107b
Devesh Varshney <deveshvarshney64@gmail.com>
2026-01-15 14:22:22 +0530

    Second commit

--- index.js
+++ index.js
@@ -1,1 +1,13 @@
-console.log("hi");
+//1 Hello World
+console.log("Node.js Backend started"); // Hello World 
+
+// 2 Variables
+let name = "Devesh";  //String
+let age = 19;         //number
+const country = "India"; //constant 
+let isStudent = true;  //boolean 
+
+console.log(name);
+console.log(age);
+console.log(country);
+console.log(isStudent);
