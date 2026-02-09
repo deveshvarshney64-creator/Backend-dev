17a0c35e2d99eee4bb901f9a17e1f1d2712ca465
Devesh Varshney <deveshvarshney64@gmail.com>
2026-02-09 16:14:05 +0530

    [Devesh] NodeLab first commit

--- index.js
+++ index.js
@@ -1,18 +1,7 @@
-//  import registerUser from "./src/registerUser.js";
-// registerUser("Devesh", "deveshvarshney64@gmail.com", "1234")
-
-//  import loginUser from "./src/loginUser.js";
-// loginUser("Devesh", "deveshvarshney64@gmail.com", "1234")
-
-// import createTodo from "./src/createTodo.js";
-// createTodo("Devesh", "deveshvarshney64@gmail.com", "1234")
-
-import logger from "./logger.js";
-
-import express from "express"
-import cors from "cors";
-import logger from "./logger/logger.js";
-let app = express()
-let port = 8080
-app.use(express.json)
-app.use(cors())
+import dotenv from "dotenv";
+dotenv.config();
+let port = process.env.PORT || 8000
+    app.get("/", (req, res)=>{
+    res.send("<h1> Hii food app is running")
+})
+app.use("/user", userRouter)
